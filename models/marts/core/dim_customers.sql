55190d3581fd0753dcbe327c9bd49fefe442cc10
Hank Ehly <henry.ehly@gmail.com>
2023-09-02 04:30:40 +0000

    Configure and select from sources

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -6,8 +6,9 @@ with
             customer_id,
             min(order_date) as first_order_date,
             max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
-        from orders
+            count(order_id) as number_of_orders,
+            sum(amount) as lifetime_value
+        from {{ ref("fct_orders") }}
         group by customer_id
     ),
     final as (
@@ -17,6 +18,7 @@ with
             customers.last_name,
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
+            coalesce(customer_orders.lifetime_value, 0) as lifetime_value,
             coalesce(customer_orders.number_of_orders, 0) as number_of_orders
         from customers
         left join customer_orders using (customer_id)
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,2 +1,9 @@
-select id as payment_id, orderid as order_id, paymentmethod as payment_method, amount, status, created
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    -- amount is stored as cents, convert to dollars
+    amount / 100 as amount,
+    status,
+    created
 from `dbt-tutorial.stripe.payment`
